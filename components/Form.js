70bc1856a3262b8b070c23b35d940b3e9946bde6
Sai <saicharan13@gmail.com>
2021-08-30 21:43:07 +0530

    Change class to className

--- components/Form.js
+++ components/Form.js
@@ -89,22 +89,22 @@ const Form = forwardRef(({ setScreenshot, resultRef }, ref) => {
             ${isProcessing && '!visible'}`}
         >
           <svg
-            class="animate-spin ml-2 mr-3 h-5 w-5 text-white md:w-6 md:h-6"
+            className="animate-spin ml-2 mr-3 h-5 w-5 text-white md:w-6 md:h-6"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             aria-label="Processing"
           >
             <circle
-              class="opacity-25"
+              className="opacity-25"
               cx="12"
               cy="12"
               r="10"
               stroke="currentColor"
-              stroke-width="4"
+              strokeWidth="4"
             ></circle>
             <path
-              class="opacity-75"
+              className="opacity-75"
               fill="currentColor"
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
